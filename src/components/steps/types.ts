3a5978a564b103cd35a40b2b1c0f56d3c6a89f26
Daniel Quinn <danjquinn42@gmail.com>
2025-08-16 21:48:14 -0400

    Update types.ts
    type fix

--- src/components/steps/types.ts
+++ src/components/steps/types.ts
@@ -6,3 +6,14 @@ export interface StepDef {
   // Vue component to render this step’s content
   component: any;
 }
+
+export type ToeUpValues = {
+  castOnStitches: number;
+  sockStitches: number;
+  heelAndGussetLength: number;
+  gussetStitchesPerSide: number;
+  stitchCountBeforeHeelTurn: number;
+  heelStitches: number;
+  heelBaseStitches: number;
+  legStartStitches: number;
+};
